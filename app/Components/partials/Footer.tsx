46379b8e1eae5194d7bd742148c5221d2bd184b7
BAssal <baslm8496@gmail.com>
2024-02-26 20:16:09 +0200

    updating path in footer

--- app/Components/partials/Footer.tsx
+++ app/Components/partials/Footer.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import { footerLinks } from "@/Constants";
+import { footerLinks } from "@app/Constants";
 
 function Footer() {
   return (
